64d3dacbaccc12c42aeaddb73277f75eb77982a2
ninjaparade <yazjallad@gmail.com>
2016-01-13 14:47:38 -0800

    added profile model

---
+++ src/Models/Profile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pta\Profile\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class profile extends Model
+{
+	protected $table = 'profiles';
+
+	protected $guarded = ['id'];
+	
+}
