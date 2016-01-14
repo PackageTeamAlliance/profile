cd24c2512a6690767a1ba76215944696a147d3cd
ninjaparade <yazjallad@gmail.com>
2016-01-13 16:21:09 -0800

    controller stubf

---
+++ src/Http/Controllers/Admin/ContentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pta\Profile\Http\Controllers\Admin;
+
+use Pta\Profile\Http\Controllers\Controller;
+
+class ContentController extends Controller
+{
+    public function __construct()
+    {
+    }
+}
